82cf8433df2aba45ec61a7ffd976067a6f11caf7
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-04 00:57:30 +0700

    Lab01_Task6_01

--- test.js
+++ test.js
@@ -1,4 +1,39 @@
-console.log('hello world')
-let x = 5;
-let y = 10;
-console.log(x+y);
+//1.ลบข้อมูลเดิมในไฟล์ Test.js
+
+//2.เขียน code if แล้วทดลองรันโปรแกรม
+// let x = 5;
+// if (x > 10){
+//     console.log('x is more than 10');
+// }
+
+//3.ลองเปลี่ยนค่า x 
+// let x = 15;
+// if (x > 10){
+//     console.log('x is more than 10');
+// }
+
+//4.ทดลองเขียน if-else statement 
+// let x = 9;
+// if (x > 10){
+//     console.log('x is more than 10');
+// }else{
+//     console.log('x is less than 10');
+// }
+
+//5.เปลี่ยนค่า x 
+// let x = 200;
+// if (x > 10){
+//     console.log('x is more than 10');
+// }else{
+//     console.log('x is less than 10');
+// }
+
+//6.ทดลองเขียนโปรแกรม 
+let x = 20;
+let y = 50;
+if (x > 5 && y > 12){
+    console.log('ค่ามากจัง')
+}else{
+    console.log(x * y + x / y)   
+    }
+
