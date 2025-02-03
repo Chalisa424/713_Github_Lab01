5d6bdce558fc0799c9718e5870e0f2c151b8d1d8
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-04 01:09:20 +0700

    Lab01_Task6_02

--- test.js
+++ test.js
@@ -4,13 +4,13 @@
 // let x = 5;
 // if (x > 10){
 //     console.log('x is more than 10');
-// }
+// } //คำตอบ ไม่แสดงค่า
 
 //3.ลองเปลี่ยนค่า x 
 // let x = 15;
 // if (x > 10){
 //     console.log('x is more than 10');
-// }
+// } //คำตอบ x is more than 10
 
 //4.ทดลองเขียน if-else statement 
 // let x = 9;
@@ -18,7 +18,7 @@
 //     console.log('x is more than 10');
 // }else{
 //     console.log('x is less than 10');
-// }
+// } //คำตอบ x is less than 10
 
 //5.เปลี่ยนค่า x 
 // let x = 200;
@@ -26,14 +26,37 @@
 //     console.log('x is more than 10');
 // }else{
 //     console.log('x is less than 10');
-// }
+// } //คำตอบ x is more than 10
 
 //6.ทดลองเขียนโปรแกรม 
-let x = 20;
-let y = 50;
-if (x > 5 && y > 12){
-    console.log('ค่ามากจัง')
+// let x = 20;
+// let y = 50;
+// if (x > 5 && y > 12){
+//     console.log('ค่ามากจัง')
+// }else{
+//     console.log(x * y + x / y)   
+//     } //คำตอบ ค่ามากจัง
+
+//8.เขียน if-else statement
+// let x = 7;
+// if (x > 10){
+//     console.log('x is more than 10')
+// }else if(x < 5){
+//     console.log('x is less than 5')
+// }else{
+//     console.log('x is between 5 and 10')
+// } //คำตอบ x is between 5 and 10
+
+//9.เขียนโปรแกรมเพื่อแสดงผลการแสดงเกรดของนักศึกษา 
+let score = 75;
+if (score > 80){
+    console.log('Grade A')
+}else if (score > 60){
+    console.log('Grade B')   
+}else if (score > 50){
+    console.log('Grade C')   
 }else{
-    console.log(x * y + x / y)   
-    }
+    console.log('Grade F') 
+} //คำตอบ Grade B
+
 
